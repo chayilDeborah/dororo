7a8bd9c91f21938f6be227e2c441879f0620733a
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-05-03 04:11:38 +0100

    feat: CA  component: mobile

--- app/components/Homepage.tsx
+++ app/components/Homepage.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
 import First from './First'
+import Ca from './Ca'
 
 const Homepage = () => {
   return (
     <main className='w-full  mx-auto '>
       <Navbar />
       <First />
+      <Ca />
     </main>
   )
 }
